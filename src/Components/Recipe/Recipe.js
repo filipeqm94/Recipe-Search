50e078875aee3bd9805191bfea8a0f59a403db3a
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-04 15:53:21 -0500

    add single recipe page styling

--- src/Components/Recipe/Recipe.js
+++ src/Components/Recipe/Recipe.js
@@ -1,27 +1,55 @@
 import React, { useContext } from 'react';
 import { DataContext } from '../../DataContext';
 
+import { Card, Container } from 'react-bootstrap';
+
 export default function Recipe() {
   const { recipe } = useContext(DataContext);
-  return (
-    <div>
-      <img src={recipe.images.SMALL.url} alt={recipe.label} />
-      <h1>{recipe.label} </h1>
-      {recipe.totalTime ? (
-        <small>Preparation Time: {recipe.totalTime} minutes</small>
-      ) : null}
 
-      <ul>
-        {recipe.ingredientLines.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
-      <h3>
-        Instruction at:{' '}
-        <a href={recipe.url} target=' _blank'>
-          {recipe.source}
-        </a>
-      </h3>
-    </div>
+  if (!recipe) {
+    return <h1>Loading</h1>;
+  }
+
+  const currentRecipe = recipe.recipe;
+  return (
+    <Container>
+      <Card className='h-100 overflow-auto text-center p-3'>
+        <Card.Title>{currentRecipe.label}</Card.Title>
+        <Card.Img
+          className='rounded mx-auto d-block'
+          src={
+            currentRecipe.images.SMALL
+              ? currentRecipe.images.SMALL.url
+              : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOFRNqBzy3DZlfFl70XxQG1kZiaCPfntvY1w&usqp=CAU'
+          }
+          alt={currentRecipe.label}
+          style={{ width: 200, height: 200 }}
+        />
+        {currentRecipe.totalTime ? (
+          <small className='d-block'>
+            Average prep time: {currentRecipe.totalTime} minutes.
+          </small>
+        ) : null}
+        <small className='d-block'>{currentRecipe.yield} servings.</small>
+        <small className='d-block'>
+          Calories per serving:{' '}
+          {(+currentRecipe.calories / +currentRecipe.yield).toFixed(2)}
+        </small>
+        <h5 className='mt-4'>Ingredients:</h5>
+        <ul className='list-group'>
+          {currentRecipe.ingredientLines.map((ingredient, index) => (
+            <li className='list-group-item' key={index}>
+              {ingredient}
+            </li>
+          ))}
+        </ul>
+        <h1>
+          Instructions can be found at:{' '}
+          <a href={currentRecipe.url} target=' _blank'>
+            {currentRecipe.source}
+          </a>
+        </h1>
+      </Card>
+    </Container>
   );
 }
