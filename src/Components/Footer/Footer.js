d93867dd11a77df98be6b3647af3c04256e1affc
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-04 15:29:02 -0500

    add footer with copyright and links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer component</h1>
-    </div>
+    <footer className='text-center fixed-bottom p-3'>
+      <small className='d-block'>&copy; Filipe Marques, 2021</small>
+      <a
+        href='https://www.linkedin.com/in/filipe-marques-518bb61ba/'
+        target=' _blank'
+      >
+        <FaLinkedin />
+      </a>
+      <a href='https://github.com/filipeqm94' target=' _blank'>
+        <FaGithub />
+      </a>
+      <a href='https://twitter.com/filipeiscoding' target=' _blank'>
+        <FaTwitter />
+      </a>
+      <a href='mailto:marques.filipe@gmail.com'>
+        <FiMail />
+      </a>
+    </footer>
   );
 }
