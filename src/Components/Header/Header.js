ba6c83f37c982f344e3dc256e02f3be2e478ecda
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-05 20:24:42 -0500

    add react boostrap navbar and filter component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import Nav from '../Nav/Nav';
+import { Navbar, Container, Offcanvas } from 'react-bootstrap';
+import Filter from '../Filter/Filter';
 
 export default function Header() {
   return (
-    <header>
-      <Nav />
-    </header>
+    <Navbar bg='warning' expand={false}>
+      <Container fluid>
+        <Link to='/'>
+          <h1>Recipe Search</h1>
+        </Link>
+        <Navbar.Toggle aria-controls='offcanvasNavbar' />
+        <Navbar.Offcanvas
+          id='offcanvasNavbar'
+          aria-labelledby='offcanvasNavbarLabel'
+          placement='end'
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id='offcanvasNavbarLabel'>
+              Search Recipes Here
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Filter />
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
   );
 }
