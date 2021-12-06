c838bed397197754c4d13ea972a3963ed0b2408f
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-05 21:42:56 -0500

    add a bit of padding to mah boi

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { FiMail } from 'react-icons/fi';
 
 export default function Footer() {
   return (
-    <footer className='text-center fixed-bottom'>
+    <footer className='text-center fixed-bottom p-1'>
       <small className='d-block'>
         &copy; Filipe Marques, 2021 -{' '}
         <span id='edamam-badge' data-color='transparent'></span>
