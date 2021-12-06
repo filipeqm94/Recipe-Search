1e02438be2ba1b8af1086041cbefbf69dc20b27c
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-05 20:02:23 -0500

    shoutout to edaman for the api

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,10 @@ import { FiMail } from 'react-icons/fi';
 export default function Footer() {
   return (
     <footer className='text-center fixed-bottom pt-3'>
-      <small className='d-block'>&copy; Filipe Marques, 2021</small>
+      <small className='d-block'>
+        &copy; Filipe Marques, 2021 -{' '}
+        <span id='edamam-badge' data-color='transparent'></span>
+      </small>
       <a
         href='https://www.linkedin.com/in/filipe-marques-518bb61ba/'
         target=' _blank'
