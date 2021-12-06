cae90966483bc6620c07d93c4f5365b5cdc1ab53
filipeqm94 <queiroz.filipe94@gmail.com>
2021-12-05 21:31:47 -0500

    sticky footer it is...

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { FiMail } from 'react-icons/fi';
 
 export default function Footer() {
   return (
-    <footer className='text-center'>
+    <footer className='text-center fixed-bottom'>
       <small className='d-block'>
         &copy; Filipe Marques, 2021 -{' '}
         <span id='edamam-badge' data-color='transparent'></span>
